bd6f4b08a5f1452de81ff78b4d7a9821f9466d70
zoltan-sipeki-admin <sipeki.zoltan10@gmail.com>
2024-10-11 16:53:00 +0200

    .

--- src/main/java/hu/cubix/hr/zoltan_sipeki/service/CompanyService.java
+++ src/main/java/hu/cubix/hr/zoltan_sipeki/service/CompanyService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import hu.cubix.hr.zoltan_sipeki.exception.CompanyAlreadyExistsException;
 import hu.cubix.hr.zoltan_sipeki.exception.CompanyNotFoundException;
