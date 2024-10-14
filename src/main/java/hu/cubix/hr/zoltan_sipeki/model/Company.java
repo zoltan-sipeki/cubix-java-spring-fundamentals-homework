b81c32dd38e4b3f950d31ce3da7faa19602e59ba
zoltan-sipeki-admin <sipeki.zoltan10@gmail.com>
2024-10-14 12:25:38 +0200

    week 5 commit

--- src/main/java/hu/cubix/hr/zoltan_sipeki/model/Company.java
+++ src/main/java/hu/cubix/hr/zoltan_sipeki/model/Company.java
@@ -2,6 +2,7 @@ package hu.cubix.hr.zoltan_sipeki.model;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -19,7 +20,7 @@ public class Company {
     
     String address;
     
-    @OneToMany(mappedBy = "company")
+    @OneToMany(mappedBy = "company", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     List<Employee> employees;
 
     public Company() {
